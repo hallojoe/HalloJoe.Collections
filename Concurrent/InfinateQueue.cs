1b6859e3655c9e945f92903415efc0e72baecc11
hallojoe <casper.korsgaard@outlook.com>
2017-01-18 02:27:49 +0100

    Init splines

---
+++ Concurrent/InfinateQueue.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Concurrent;
+namespace HalloJoe.Collections.Concurrent
+{
+    public class InfiniteQueue<T> : ConcurrentQueue<T>
+    {
+        public new bool TryDequeue(out T result)
+        {
+            if (base.TryDequeue(out result))
+            {
+                Enqueue(result);
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ Generic/InfinateQueue.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+namespace HalloJoe.Collections.Generic
+{
+    public class InfiniteQueue<T> : Queue<T>
+    {
+        public new T Dequeue()
+        {
+            T item = base.Dequeue();
+            Enqueue(item);
+            return item;
+        }
+    }
+}
